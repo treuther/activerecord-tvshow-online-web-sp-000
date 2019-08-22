6d0584b62dcc067c22842a9acd3cbb9e45bfbc53
Tracy Reuther <tracyreuther@gmail.com>
2019-08-22 00:26:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
 
 #return the highest value in the ratings column. hint: if there is a minimum
 #Active Record method, might there be a maximum method?
-def highest_rating
+def self.highest_rating
 Show.maximum(:rating)
 end
 
