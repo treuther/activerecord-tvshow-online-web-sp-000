b16adacd39fd8f62d81116ca696ab16cfbf4670b
Tracy Reuther <tracyreuther@gmail.com>
2019-08-22 00:00:43 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,5 +1,9 @@
 class CreateShows > ActiveRecord::Migration[5.2]
   def change
-    create_table :shows
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
   end
 end
